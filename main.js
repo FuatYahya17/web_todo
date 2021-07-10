294b458dd3e9a9fb5c266317375a009121b7ca05
jordanos <y.dereje@yahoo.com>
2021-07-10 14:43:01 +0300

    styled the components

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+// This function gets a text input from the textarea element and update the notes
+
 function add_todo() {
+  // get note from textarea element
   let note = document.getElementById("todo-text").value;
   if (true) {
+    // get bunch of notes from the notes
     let notes = document.getElementById("notes").innerHTML;
+
+    // add the new note to the previous notes
     new_notes =
       notes +
       `<div class="note">
@@ -10,6 +16,14 @@ function add_todo() {
       `</p>
         <button class="btn remark">Done</button>
       </div>`;
+
+    // Update notes
     document.getElementById("notes").innerHTML = new_notes;
+
+    // Delete everything from the textarea input making it ready for the next input
+    document.getElementById("todo-text").value = "";
   }
 }
+
+// TODO
+// When button done is pressed remove the item from the notes list
